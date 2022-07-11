edea0c0d1578cdb568f5f5bb87997238520c1fe6
conquy2002 <72489231+conquy2002@users.noreply.github.com>
2022-07-11 10:48:13 +0700

    Initial commit

---
+++ src/main/java/com/example/can_bac_2/CanBac2Application.java
@@ -0,0 +1,13 @@
+package com.example.can_bac_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanBac2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanBac2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/can_bac_2/CanBac2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.can_bac_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanBac2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
