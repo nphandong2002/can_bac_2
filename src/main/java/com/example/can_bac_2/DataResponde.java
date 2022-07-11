6f6dd7d6039057a576bb73c7809db5d063783c26
conquy2002 <72489231+conquy2002@users.noreply.github.com>
2022-07-11 10:48:33 +0700

    new

--- src/main/java/com/example/can_bac_2/DataResponde.java
+++ src/main/java/com/example/can_bac_2/DataResponde.java
@@ -1,2 +1,40 @@
-package com.example.can_bac_2;public class DataResponde {
+package com.example.can_bac_2;
+
+public class DataResponde {
+    private String msg;
+    private float x1;
+    private float x2;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public float getX1() {
+        return x1;
+    }
+
+    public void setX1(float x1) {
+        this.x1 = x1;
+    }
+
+    public float getX2() {
+        return x2;
+    }
+
+    public void setX2(float x2) {
+        this.x2 = x2;
+    }
+    public DataResponde(){
+
+    }
+    public DataResponde(String msg, float x1 , float x2){
+        this.msg = msg;
+        this.x1 = x1;
+        this.x2 = x2;
+    }
+
 }
--- src/main/java/com/example/can_bac_2/mainapp.java
+++ src/main/java/com/example/can_bac_2/mainapp.java
@@ -1,2 +1,24 @@
-package com.example.can_bac_2;public class mainapp {
+package com.example.can_bac_2;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/api")
+public class mainapp {
+    @GetMapping
+    public ResponseEntity canbac2(@RequestParam("a") int a ,@RequestParam("b") int b ,@RequestParam("c") int c ){
+        int denta = b*b - 4 * a * c;
+        if(denta < 0) return  ResponseEntity.ok(new DataResponde("Phuong trinh vo nghiem",0,0));
+        if(denta == 0){
+            int x = -b / 2 * a;
+            return  ResponseEntity.ok(new DataResponde("Phuong trinh co nghiem kep",x,x));
+        }
+        double candenta = Math.sqrt(denta);
+        float cd = (float) candenta;
+        return  ResponseEntity.ok(new DataResponde("Phuong trinh co nghiem kep",(-b + cd)/2* a,(-b - cd)/2* a));
+    }
 }
