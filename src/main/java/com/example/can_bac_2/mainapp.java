d6a90604f7a5e491f3e274f18a0b83b74d1aa130
conquy2002 <72489231+conquy2002@users.noreply.github.com>
2022-07-11 14:22:59 +0700

    new

--- src/main/java/com/example/can_bac_2/mainapp.java
+++ src/main/java/com/example/can_bac_2/mainapp.java
@@ -11,6 +11,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class mainapp {
     @GetMapping
     public ResponseEntity canbac2(@RequestParam("a") int a ,@RequestParam("b") int b ,@RequestParam("c") int c ){
+
+        if(a == 0){
+            if(b == 0 && c != 0){
+                return  ResponseEntity.ok(new DataResponde("Phuong trinh vo nghiem",0,0));
+            }
+            if(b == 0 && c == 0){
+                return  ResponseEntity.ok(new DataResponde("Phuong trinh vo so nghiem",0,0));
+            }
+            float x = (0-c)/ b;
+            System.out.println(x);
+            return  ResponseEntity.ok(new DataResponde("Phuong trinh co nghiem",x,0));
+
+        }
         int denta = b*b - 4 * a * c;
         if(denta < 0) return  ResponseEntity.ok(new DataResponde("Phuong trinh vo nghiem",0,0));
         if(denta == 0){
@@ -18,7 +31,8 @@ public class mainapp {
             return  ResponseEntity.ok(new DataResponde("Phuong trinh co nghiem kep",x,x));
         }
         double candenta = Math.sqrt(denta);
+
         float cd = (float) candenta;
-        return  ResponseEntity.ok(new DataResponde("Phuong trinh co nghiem kep",(-b + cd)/2* a,(-b - cd)/2* a));
+        return  ResponseEntity.ok(new DataResponde("Phuong trinh co ",(-b + cd)/2* a,(-b - cd)/2* a));
     }
 }
